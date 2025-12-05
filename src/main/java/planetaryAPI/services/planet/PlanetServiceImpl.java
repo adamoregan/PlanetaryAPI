4fffcd65bba389986f4c5a7faec472b0c27ce3e5
adamoregan <adamoregan457@gmail.com>
2025-12-05 17:11:50 +0000

    Shorten interface specifier

--- src/main/java/planetaryAPI/services/planet/PlanetServiceImpl.java
+++ src/main/java/planetaryAPI/services/planet/PlanetServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class PlanetServiceImpl implements planetaryAPI.services.planet.PlanetService {
+public class PlanetServiceImpl implements PlanetService {
     private PlanetRepository planetRepository;
 
     @Override
