776db6fe76780233cee13ff5a8e3c8f67933f22d
adamoregan <adamoregan457@gmail.com>
2025-12-09 10:59:34 +0000

    Change interface method argument name

--- src/main/java/planetaryAPI/services/user/APIUserService.java
+++ src/main/java/planetaryAPI/services/user/APIUserService.java
@@ -3,5 +3,5 @@ package planetaryAPI.services.user;
 import planetaryAPI.dtos.user.APIUserDTO;
 
 public interface APIUserService {
-    APIUserDTO findUserByName(String id);
+    APIUserDTO findUserByName(String name);
 }
